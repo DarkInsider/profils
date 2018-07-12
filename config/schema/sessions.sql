73cd3293200493b53b699368c416fbfbc8ecbf90
DarkInsider <40956662+DarkInsider@users.noreply.github.com>
2018-07-12 16:24:23 +0300

    Update sessions.sql

--- config/schema/sessions.sql
+++ config/schema/sessions.sql
@@ -13,3 +13,167 @@ CREATE TABLE `sessions` (
   `expires` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `cities`
+--
+
+CREATE TABLE `cities` (
+  `id` int(11) NOT NULL,
+  `city` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `country_code` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `cities`
+--
+
+INSERT INTO `cities` (`id`, `city`, `country_code`) VALUES
+(1, 'Kiev', 2),
+(2, 'Kirovograd', 2),
+(3, 'New York', 1),
+(4, 'Paris', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `countries`
+--
+
+CREATE TABLE `countries` (
+  `id` int(11) NOT NULL,
+  `country` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `countries`
+--
+
+INSERT INTO `countries` (`id`, `country`) VALUES
+(1, 'USA'),
+(2, 'Ukraine'),
+(3, 'France');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `links`
+--
+
+CREATE TABLE `links` (
+  `id` int(11) NOT NULL,
+  `link` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `profil_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `links`
+--
+
+INSERT INTO `links` (`id`, `link`, `profil_id`) VALUES
+(2, 'https://www.youtube.com/watch?v=B7iyGqAKxZ8', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `profils`
+--
+
+CREATE TABLE `profils` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `surname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `country` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `city` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `profils`
+--
+
+INSERT INTO `profils` (`id`, `name`, `surname`, `phone`, `email`, `birthday`, `country`, `city`, `photo`) VALUES
+(4, 'Alexandr', 'Griboedov', '+380992281337', 'kirill2009@gmail.com', '2009-05-31', 'France', 'Paris', 'Koala.jpg312592bin'),
+(5, 'Arnold', 'Schwarzenegger', '+4291045676798335645834502394', 'arnold@schwarzenegger.com', '1948-07-10', 'USA', 'New York', '27908278_10208864505968314_6543661223403133820_o.jpg611522bin'),
+(6, 'Test FN', 'Test SN', '123456789', 'aaa@bbb.com', '2017-07-11', 'Ukraine', 'Kiev', '30dor_hubble_3939.jpg505523bin');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `cities`
+--
+ALTER TABLE `cities`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cities_ibfk_1` (`country_code`);
+
+--
+-- Индексы таблицы `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `links`
+--
+ALTER TABLE `links`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `links_ibfk_1` (`profil_id`);
+
+--
+-- Индексы таблицы `profils`
+--
+ALTER TABLE `profils`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `cities`
+--
+ALTER TABLE `cities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `countries`
+--
+ALTER TABLE `countries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `links`
+--
+ALTER TABLE `links`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `profils`
+--
+ALTER TABLE `profils`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `cities`
+--
+ALTER TABLE `cities`
+  ADD CONSTRAINT `cities_ibfk_1` FOREIGN KEY (`country_code`) REFERENCES `countries` (`id`) ON DELETE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `links`
+--
+ALTER TABLE `links`
+  ADD CONSTRAINT `links_ibfk_1` FOREIGN KEY (`profil_id`) REFERENCES `profils` (`id`) ON DELETE CASCADE;
+COMMIT;
